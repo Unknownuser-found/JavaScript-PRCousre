a51d3de5b5c5c3d73be07654dd02fd45fe33d606
elliotalderson <elliotalderson402@gmail.com>
2021-04-30 14:54:34 +0530

    All Source Code

---
+++ tut53Functions.js
@@ -0,0 +1,33 @@
+
+let name="Elliot alderson ";
+let name2="Mr. Robot ";
+let name3="Darlene ";
+let name4="Tyrel Wellick ";
+let greetText="Good Morning ";
+
+function greet(name, greetText="Good Night Mr....."){                //greettext="whatever" is the default value if it wouldn't call
+    console.log(greetText + name);
+    // console.log(name + 'Is a Naughty boy');
+}
+greet(name,greetText);
+greet(name3,greetText);
+greet(name2,greetText);
+greet(name4, );
+
+
+
+
+function sum(a,b,c) {
+    let d = a + b + c ; 
+    return d;                  //After return function is stoped
+    // console.log("Can's show the output") //This is unreachable
+}
+let returnvalue = sum(1,2,3);
+console.log(returnvalue)
+// ----------Theory---------
+//  returvalue = sum(1,2,3) so yaha pe is returnvalue variable function of call karaya and function ne apna kam kiya ki
+//  phele so hua ki 1,2,3 abc mein ghuuse and fir ek and jo d variable h isme + ho gaye and unki value return krdi
+
+
+
+
